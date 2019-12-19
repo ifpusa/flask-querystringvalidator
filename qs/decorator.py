b25ed126adb2b115e1ecf1439220e7e059221e6d
Jesse Whitehouse <jesse@whitehouse.dev>
2019-12-19 13:41:30 -0600

    Fixes behavior visa vis <select> fields versus CSV input to <input type="text"> fields. Adds comments for clarity of this behavior.

--- qs/decorator.py
+++ qs/decorator.py
@@ -7,9 +7,12 @@ from collections import namedtuple
 
 from . import fields
 
+def first(iterable):
+
+	return next(iter(iterable))
 
 
-def set(defaults=None):
+def set(defaults=None, mod=None):
 
 	QueryString = namedtuple('QueryString', ' '.join(defaults.keys()))
 
@@ -19,7 +22,7 @@ def set(defaults=None):
 
 			if defaults is None:
 				return func(*args, **kwargs)
-		
+
 			fields_this_request = convert_qs_to_fields(request.args, defaults)
 
 			if not all([i in fields_this_request for i in defaults]):
@@ -35,6 +38,12 @@ def set(defaults=None):
 			# g.qs retains Field objects for use in templates and app logic
 			g.qs = QueryString(**response_fields)
 			
+			# TODO: Write a test that checks what happens when params are URL encoded
+			# 		I'd like to remove URL encoded values to make the URL more readable
+			#		but the urldecode functions in Werkzeug make that near impossible
+			#		Might want to see if I can write my own but borrow some of the test
+			#		definitions form the Werkzeug codebase.
+					
 			# If the format of request fields matches what we'd redirect to,
 			# then don't redirect, just build the page
 			if all([field_dump[k] == request.args.get(k)
@@ -44,8 +53,8 @@ def set(defaults=None):
 
 			# Otherwise, build a freshly formatted URL with all the default
 			# vars and redirect to it.
-			new_url = url_for(request.endpoint, **field_dump)
-				
+			new_url = url_for(request.endpoint, mod=mod, **field_dump)
+
 			# Unquote the URL in case special characters were %-ified
 			unquoted = unquote(new_url)
 
@@ -63,7 +72,7 @@ def convert_qs_to_fields(request_qs, default_fields):
 	It tosses key-value-pairs not defined in default_fields.
 	It converts the querystring strings into Field() objects.
 	'''
-	
+
 	parsed_fields = {}
 
 	for k in request_qs.keys():
@@ -73,12 +82,47 @@ def convert_qs_to_fields(request_qs, default_fields):
 
 		try:
 
-			if isinstance(default_fields[k], fields.ListField):
-				parsed_fields[k] = type(default_fields[k])(request_qs.getlist(k))
-			else:	
-				parsed_fields[k] = type(default_fields[k])(request_qs.get(k))
+			# ftype == field type (such as Integer, String, Date etc.)
+			ftype = type(default_fields[k])
+			_get_val = request_qs.get(k)
+
+			# Non ListField types are easy because the whole querystring
+			# value is passed to the Field constructor
+			if not isinstance(ftype(), fields.ListField):
+				parsed_fields[k] = ftype(_get_val)
+				continue
+
+			# At this point we know that the querystring schema expects
+			# a list will be passed to the constructor.
+
+			# This logic handles if a form was used to pass
+			# a multiselect. In which case the query string will
+			# have multiple key-value-pairs for each selected field
+			# value: ?field=value1&field=value2&field=value3
+
+			# It also handles requests with a hand-formed query string
+			# like this: ?value1,value2,value3
+
+			# ImmutableDict.getlist() returns a list even if a single value
+			# was present. This logic confirms that a form was used
+			# to prepare the querystring.
+
+			_getlist_val = request_qs.getlist(k)
+
+			_sent_by_hand = len(_getlist_val) == 1 and first(_getlist_val) == _get_val
+			_sent_by_form = not _sent_by_hand
+
+			if _sent_by_hand:
+				field_constructor_list = _get_val.split(',')
+			if _sent_by_form:
+				field_constructor_list = _getlist_val
+
+			if field_constructor_list == ['']:
+				parsed_fields[k] = ftype()
+			else:
+				parsed_fields[k] = ftype(field_constructor_list)
+
 		except ValueError:
 			pass
 
 	return parsed_fields
-
