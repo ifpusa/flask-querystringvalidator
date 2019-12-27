91e9f2b9e79075af2d02540e51c1912d9df5e416
Jesse Whitehouse <jesse@whitehouse.dev>
2019-12-27 08:05:17 -0600

    Bumps version number. No breaking changes to API.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="Flask-QueryStringValidator",
-    version=0.1,
+    version=0.2,
     url="https://github.com/ifpusa/flask-querystringvalidator",
     project_urls={
         "Documentation": "https://github.com/ifpusa/flask-querystringvalidator",
